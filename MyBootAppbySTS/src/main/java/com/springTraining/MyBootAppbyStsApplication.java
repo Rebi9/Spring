bd25e830e3822caadf0c3d5d22b8d3eeb506306b
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-09-09 11:18:14 +0900

    STSからプロジェクトを作成。

---
+++ MyBootAppbySTS/src/main/java/com/springTraining/MyBootAppbyStsApplication.java
@@ -0,0 +1,12 @@
+package com.springTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppbyStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppbyStsApplication.class, args);
+	}
+}
