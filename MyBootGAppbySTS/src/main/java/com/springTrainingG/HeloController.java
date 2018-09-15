e9de2ff16073fffb75243a17e9ba55974a45bb7d
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-09-15 12:32:47 +0900

    フォームを用いて、値を受け渡す。

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -1,20 +1,25 @@
 package com.springTrainingG;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-		int res = 0;
-		for(int i = 1; i <= num; i++) {
-			res += i;
-		}
-		mav.addObject("msg", "total: " + res);
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg", "名前を入力し、Clickを押してください。");
+		mav.setViewName("index");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam ("text1") String str, ModelAndView mav) {
+		mav.addObject("msg",  str + "さんは、えらい！");
+		mav.addObject("value", str);
 		mav.setViewName("index");
 		return mav;
 	}
