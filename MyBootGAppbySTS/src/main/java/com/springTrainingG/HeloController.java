057d55ac140ed4ba673b507a8d07e6fe760e5a20
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-09-18 08:41:12 +0900

    th:eachを用いて出力。

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -1,20 +1,23 @@
 package com.springTrainingG;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		int m = Math.abs(month) % 12;
-		m = m == 0 ? 12 : m;
-		mav.addObject("month", m);
-		mav.addObject("check", Math.floor(m / 3));
+		List<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"taro", "taro@dokaben", "090-9999-1111"});
+		data.add(new String[] {"hanako", "hanako@toilet", "090-2222-3333"});
+		data.add(new String[] {"takuro", "takuro@bullet", "999-9999-9999"});
+		mav.addObject("data", data);
 		return mav;
 	}
 
