d2cbbc1c76c8ab30a064f7d708ec7beceec711f2
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-17 12:16:22 +0900

    インライン処理の学習

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -4,22 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("num", num);
-		if(num >= 0) {
-			mav.addObject("check", "num >= data.size() ? 0 : num");
-		} else {
-			mav.addObject("check", "num <= data.size() * -1 ? 0 : num * -1");
-		}
 		List<DataObject> data = new ArrayList<DataObject>();
 		data.add(new DataObject(0, "taro", "taro@okamoto"));
 		data.add(new DataObject (1, "fredy", "fredy@queen"));
