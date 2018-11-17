170cf9aad102b3f491cc091ef33df9fcc8e1f7e2
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-17 16:49:06 +0900

    登録処理の学習

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -2,7 +2,10 @@ package com.springTrainingG;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springTrainingG.repositories.MyDataRepository;
@@ -13,12 +16,19 @@ public class HeloController {
 	@Autowired
 	MyDataRepository repository;
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg", "this is sample content.");
 		Iterable<MyData> list = repository.findAll();
-		mav.addObject("data", list);
+		mav.addObject("datalist", list);
 		return mav;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView form(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
 	}
 }
