73eae9a830af5286105bd2369fe9e081742700c2
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-17 11:59:39 +0900

    プリプロセッシングの学習

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -4,21 +4,65 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping("/{num}")
+	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
 		mav.setViewName("index");
-		List<String[]> data = new ArrayList<String[]>();
-		data.add(new String[] {"taro", "taro@dokaben", "090-9999-1111"});
-		data.add(new String[] {"hanako", "hanako@toilet", "090-2222-3333"});
-		data.add(new String[] {"takuro", "takuro@bullet", "999-9999-9999"});
+		mav.addObject("num", num);
+		if(num >= 0) {
+			mav.addObject("check", "num >= data.size() ? 0 : num");
+		} else {
+			mav.addObject("check", "num <= data.size() * -1 ? 0 : num * -1");
+		}
+		List<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0, "taro", "taro@okamoto"));
+		data.add(new DataObject (1, "fredy", "fredy@queen"));
+		data.add(new DataObject (2, "takuro", "takuro@bullet"));
 		mav.addObject("data", data);
 		return mav;
+	}
+
+	class DataObject {
+		private int id;
+		private String name;
+		private String mail;
+
+		public DataObject(int id, String name, String mail) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.mail = mail;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getMail() {
+			return mail;
+		}
+
+		public void setMail(String mail) {
+			this.mail = mail;
+		}
 	}
 
 }
