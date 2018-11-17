f92efd237b9c0064e4457b071df1c82d6303a53f
Ryo Ebihara <ab.yr.2t@gmail.com>
2018-11-17 12:41:29 +0900

    テンプレートフラグメントの学習

--- MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
+++ MyBootGAppbySTS/src/main/java/com/springTrainingG/HeloController.java
@@ -1,17 +1,15 @@
 package com.springTrainingG;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/{tax}")
-	public ModelAndView index(@PathVariable int tax, ModelAndView mav) {
+	@RequestMapping("")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("tax", tax);
 		return mav;
 	}
 }
